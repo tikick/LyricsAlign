4ad1e434134e8bf4e0aa2daaf2bb4759eeb4af60
tikick <91670109+tikick@users.noreply.github.com>
2024-05-13 18:16:06 +0200

    Update download.py

--- download.py
+++ download.py
@@ -1,16 +1,12 @@
 import DALI as dali_code
 import os
 from pytube import YouTube
-import numpy as np
 
 import config
-from utils import load
 
 
 def download_dali_audio():
-    dali_data = dali_code.get_the_DALI_dataset(config.dali_annotations, skip=[],
-                                               #keep=[])
-                                               keep=['0a3cd469757e470389178d44808273ab'])
+    dali_data = dali_code.get_the_DALI_dataset(config.dali_annotations, skip=[], keep=[])
 
     base_url = 'https://www.youtube.com/watch?v='
     lang = 'english'
@@ -35,13 +31,10 @@ def download_dali_audio():
             num_downloads += 1
         except Exception as e:
             num_fails += 1
-            print(f'Failed to download dali_id={dali_id}, url={url}: {repr(e)}')
+            print(f'Failed to download dali_id = {dali_id}, url = {url}: {repr(e)}')
 
     print(f'Successfully donwloaded {num_downloads} / {num_downloads + num_fails} songs')
 
 
 if __name__ == '__main__':
     download_dali_audio()
-    audio_path = os.path.join(config.dali_audio, '0a3cd469757e470389178d44808273ab.mp3')
-    waveform = load(audio_path, sr=config.sr)
-    print(np.max(waveform))
