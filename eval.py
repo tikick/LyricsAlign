39f073480bcf167dbd739a4206a20b96c8094289
tikick <91670109+tikick@users.noreply.github.com>
2024-05-17 18:54:30 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -21,10 +21,7 @@ def evaluate(model, device, jamendo):  #, metric='PCO'):
 
     with torch.no_grad():
         for song in jamendo:
-            print('song:\n')
-            print(song['id'])
-            print(song['words'])
-            print(song['gt_alignment'])
+            print(f'song:\n{song['id']}\n{song['words']}\n{song['gt_alignment']}')
             spectrogram, positives = jamendo_collate(song)
             spectrogram, positives = spectrogram.to(device), positives.to(device)
 
