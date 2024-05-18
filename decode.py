0a2a25e15a9e93a4088e67a8fd152c1c6f205d75
tikick <91670109+tikick@users.noreply.github.com>
2024-05-18 18:26:45 +0200

    Update decode.py

--- decode.py
+++ decode.py
@@ -94,11 +94,10 @@ def _align(S, song, level='word'):
         #ax.set_yticklabels(ytick_labels, fontsize=8)
     
     matrix_cmap = 'hot'
-    alignment_cmap = 'hot'
+    alignment_cmap = 'GnBu'
 
     r = len(tokens) // len(song['words'])
-    fig, axs = plt.subplots(5, 1, height_ratios=[r, r, r, 1, 1])
-    fig.tight_layout()
+    fig, axs = plt.subplots(5, 1, height_ratios=[r, r, r, 1, 1], figsize=(16, 18))
     show(S, axs[0], 'S', tokens, cmap=matrix_cmap, cbar=True)
     show(DP, axs[1], 'DP', tokens, cmap=matrix_cmap, cbar=True)
     show(token_alignment_image, axs[2], 'token alignment', tokens, cmap=alignment_cmap)
