9eab08b434fcd80fcaa6f533c2d9d8dbda95d517
tikick <91670109+tikick@users.noreply.github.com>
2024-05-20 10:25:28 +0200

    Update decode.py

--- decode.py
+++ decode.py
@@ -87,7 +87,8 @@ def _align(S, song, level='word'):
     alignment_cmap = 'Blues'
     tokens = encode_words(song['words'], space_padding=1) if config.use_chars else encode_phowords(song['phowords'], space_padding=1)    
     heights = [len(tokens)] * 3 + [len(song['words'])] * 2
-    fig, axs = plt.subplots(5, 1, height_ratios=heights, figsize=(num_frames // 14, (sum(heights) + 90) // 12))
+    fig, axs = plt.subplots(5, 1, height_ratios=heights, 
+                            figsize=(min(num_frames // 14, 680), min((sum(heights) + 90) // 12, 680)))
     show(S, axs[0], 'S', tokens, matrix_cmap)
     show(DP, axs[1], 'DP', tokens, matrix_cmap)
     show(token_alignment_image, axs[2], 'token alignment', tokens, alignment_cmap)
