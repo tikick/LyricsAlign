d00fa31bc02f7ebbb24e638f446210fc1a9cc796
tikick <91670109+tikick@users.noreply.github.com>
2024-06-01 15:57:34 +0200

    use_chars = True

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ channels = 64
 
 ##### text encoder
 context = 1
-use_chars = False  # if false uses phonemes
+use_chars = True  # if false uses phonemes
 vocab_size = 28 if use_chars else 40
 
 ##### optimizer, data loader and others
