4eea1c666301aeedc897b2a78c087fe0159b9690
tikick <91670109+tikick@users.noreply.github.com>
2024-06-09 15:34:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ jamendo_segments_audio = os.path.join(jamendo_segments_base, 'mp3')
 embedding_dim = 64
 
 ##### audio encoder
-num_RCBs = 5
+num_RCBs = 10
 channels = 64
 
 ##### text encoder
@@ -44,7 +44,7 @@ vocab_size = 28 if use_chars else 40
 
 ##### optimizer, data loader and others
 num_epochs = 20
-lr = 1e-4
+lr = 0.001
 batch_size = 64
 
 
