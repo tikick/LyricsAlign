9a84e52f4a78ccbbdde537ede0755c04e813e810
tikick <91670109+tikick@users.noreply.github.com>
2024-06-13 11:48:45 +0200

    georg pytube_files

--- georg.py
+++ georg.py
@@ -3,8 +3,8 @@ import os
 import pandas as pd
 
 with open(os.path.join(config.georg_base, 'data', 'pytube_files.txt'), 'w') as f:
-    for i in range(1):
+    for i in range(20):
         parq_file = os.path.join(config.georg_annotations, str(i), 'alignment.parq')
         df = pd.read_parquet(parq_file, engine='pyarrow')
         for _, row in df.iterrows():
-                f.write(row['ytid'] + '\n')
+                f.write(row['ytid'] + '.mp3\n')
