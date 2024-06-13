1c29f37f5804c2821f6a73f91ea22f4eee5d8ab9
tikick <91670109+tikick@users.noreply.github.com>
2024-06-13 12:04:02 +0200

    Update georg.py

--- georg.py
+++ georg.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 with open(os.path.join(config.georg_base, 'data', 'pytube_files.txt'), 'w') as f:
-    for i in range(20):
+    for i in range(1):
         parq_file = os.path.join(config.georg_annotations, str(i), 'alignment.parq')
         df = pd.read_parquet(parq_file, engine='pyarrow')
         for _, row in df.iterrows():
