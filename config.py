5a19ba71404427e3ac403969018ef2a847a14571
tikick <91670109+tikick@users.noreply.github.com>
2024-06-17 11:23:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ vocab_size = 28 if use_chars else 40  # len(char_dict) if use_chars else len(pho
 embedding_dim = 64
 
 ### others:
-num_epochs = 20
+num_epochs = 4
 lr = 0.0001
 batch_size = 32
 num_negative_samples = 1_000
