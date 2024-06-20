7e4865c2823f0fea559738a9a68002c83e68dfb5
tikick <91670109+tikick@users.noreply.github.com>
2024-06-20 18:29:48 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -85,8 +85,8 @@ if __name__ == '__main__':
     jamendo = get_jamendo()
     jamendoshorts = get_jamendoshorts()
 
-    for epoch in range(10, 20):
-        model.load_state_dict(torch.load(os.path.join(config.checkpoint_dir, '06-17,11:55', str(epoch))))
+    for epoch in range(2, 4):
+        model.load_state_dict(torch.load(os.path.join(config.checkpoint_dir, '06-17,12:30', str(epoch))))
 
         evaluate(model, device, jamendoshorts, log=True, epoch=epoch)
         PCO_jamendo, AAE_jamendo = evaluate(model, device, jamendo, log=False)
