c65893f90fd608c926ddab6ae3f37fa4faf5238a
tikick <91670109+tikick@users.noreply.github.com>
2024-06-23 17:19:51 +0200

    Update georg.py

--- georg.py
+++ georg.py
@@ -1,10 +1,7 @@
 import config
 import os
-import pandas as pd
 
-with open(os.path.join(config.georg_base, 'data', 'audio_ids.txt'), 'w') as f:
-    for i in range(20):
-        parq_file = os.path.join(config.georg_annotations, str(i), 'alignment.parq')
-        df = pd.read_parquet(parq_file, engine='pyarrow')
-        for _, row in df.iterrows():
-                f.write(row['ytid'] + '.mp3\n')
+with open(os.path.join(config.georg_base, 'data', 'audio_ids.txt'), 'r') as r:
+    with open(os.path.join(config.georg_base, 'data', 'vocals_ids.txt'), 'w') as w:
+        for line in r.readlines():
+            w.write(line[:-4] + '_vocals.mp3\n')
