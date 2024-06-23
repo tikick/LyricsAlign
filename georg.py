341621a048879e8a5d8a0dad3771cb10bcdb2df3
tikick <91670109+tikick@users.noreply.github.com>
2024-06-23 17:24:24 +0200

    Update georg.py

--- georg.py
+++ georg.py
@@ -4,4 +4,4 @@ import os
 with open(os.path.join(config.georg_base, 'data', 'audio_ids.txt'), 'r') as r:
     with open(os.path.join(config.georg_base, 'data', 'vocals_ids.txt'), 'w') as w:
         for line in r.readlines():
-            w.write(line[:-4] + '_vocals.mp3\n')
+            w.write(line[:-5] + '_vocals.mp3\n')
